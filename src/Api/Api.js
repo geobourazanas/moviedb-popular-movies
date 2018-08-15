831446b52ab6a1566a8b5d7faf790e413235859a
George Bourazanas <george@bourazanas.com>
2018-08-15 22:00:35 +0100

    Update Movies component logic with rxjs observables

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,4 +1,19 @@
 import axios from 'axios';
+import {
+  Observable,
+  Subject,
+  from,
+  of,
+  empty,
+} from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  switchMap,
+} from 'rxjs/operators';
 
 class API {
   constructor() {
@@ -7,6 +22,8 @@ class API {
       base_uri: 'https://api.themoviedb.org/3/',
       images_uri: 'http://image.tmdb.org/t/p/',
     }
+
+    this.searchTerm = new Subject();
   }
 
   request (url, method = 'GET', extra_params = {}) {
@@ -20,8 +37,27 @@ class API {
     });
   }
 
-  getMostPopular() {
-    return this.request('discover/movie', undefined, {sort_by: 'popularity.desc'});
+  search(term) {
+    this.searchTerm.next(term.value);
+  }
+
+  doSearch(term) {
+    return from(this.request('search/movie', undefined, {query: term}));
+  }
+
+  getResults() {
+    return this.searchTerm
+                .pipe(
+                  debounceTime(500),
+                  distinctUntilChanged(),
+                  switchMap(term => term
+                    ? this.doSearch(term) : empty()),
+                  map(res => res.data.results),
+                  catchError(error => {
+                    console.error(error);
+                    return empty();
+                  }),
+                );
   }
 
 };
--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,34 +1,75 @@
 import React from 'react';
 import API from '../Api/Api';
-
-const Api = new API();
+import MoviesSearch from './MoviesSearch';
+import './Movies.css';
 
 class Movies extends React.Component {
 
-  state: {
-    foo: '',
+  constructor() {
+    super();
+
+    this.Api = new API();
+    this.state = {
+      searchResults: [],
+    };
   }
 
   componentWillMount() {
-    Api.getMostPopular()
-      .then(
-        (result) => {
+    this.Api
+        .getResults()
+        .subscribe(res => {
           this.setState({
-            isLoaded: true,
-            items: result.data
+            searchResults: this.sortResults(res),
           });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
+        });
+  }
+
+  handleTyping(e) {
+    const term = e.target.value.trim();
+    if (term.length >= 3) {
+      this.Api.search({ value: term });
+    } else {
+      this.emptyList();
+    }
+  }
+
+  handleClick(movie, e) {
+    e.preventDefault();
+  }
+
+  emptyList() {
+    this.setState({
+      searchResults: [],
+    });
+  }
+
+  sortResults(arr) {
+    return arr.sort((a, b) =>
+      (a.vote_count > b.vote_count) ? -1 :
+      (
+        (b.vote_count > a.vote_count) ? 1 :
+      0)
+    );
   }
 
   render() {
-    return (<h1> Hello, World!</h1>);
+    const results = this.state.searchResults && this.state.searchResults.map(movie => {
+      return <li className="MoviesSearch__list-group-item" key={movie.id}>
+                <a href="" onClick={(e) => this.handleClick(movie, e)}>{movie.title} <span>(votes: {movie.vote_count})</span></a>
+             </li>
+    });
+
+    return (
+      <div className="MoviesContainer">
+        <h1>Most Popular Movies</h1>
+        <MoviesSearch
+          onChange={this.handleTyping.bind(this)}
+        />
+        {this.state.searchResults && <ul className="MoviesSearch__list-group">
+          {results}
+        </ul>}
+      </div>
+    );
   }
 }
 
