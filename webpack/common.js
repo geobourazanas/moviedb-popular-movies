71fcf93b3d2749a246a5979acc1b93aca83f4592
George Bourazanas <george@bourazanas.com>
2018-08-31 18:21:08 +0100

    Move webpack files to each own folder

--- webpack/common.js
+++ webpack/common.js
@@ -12,7 +12,20 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: { presets: ['env'] }
+        // options: { presets: ['env'] }
+        query: {
+          presets: [
+            "env",
+            "react",
+            "stage-2"
+          ],
+          plugins: [
+            "syntax-dynamic-import",
+            "transform-class-properties",
+            "transform-es2015-destructuring",
+            "transform-object-rest-spread"
+          ]
+        }
       },
       {
         test: /\.css$/,
@@ -23,13 +36,14 @@ module.exports = {
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/')
+    path: path.resolve(__dirname, '../dist/'),
+    publicPath: '/',
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, 'public/index.html'),
+      template: path.resolve(__dirname, '../public/index.html'),
     }),
   ],
 };
